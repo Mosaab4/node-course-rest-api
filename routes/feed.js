1c9c0735e2cd2e175d9ef91b195f25226d6a3949
Mosaab <musab.ehab@gmail.com>
2023-04-22 00:34:00 +0200

    init the project

---
+++ routes/feed.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const feedController = require('../controllers/feed');
+
+const router = express.Router();
+
+router.get('/posts', feedController.getPosts);
+
+// POST /feed/post
+router.post('/post', feedController.createPost);
+
+module.exports = router;
