c1fd5e5715c3cacee01e58acb630460dad6c77c7
Mosaab <musab.ehab@gmail.com>
2023-04-24 23:36:22 +0200

    add delete and edit post & uploading images

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const path = require('path')
+const multer = require('multer')
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
 app.use(bodyParser.json()); // application/json
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
@@ -23,7 +46,7 @@ app.use((err, req, res, next) => {
     const status = err.statusCode
     const message = err.message
 
-    res.status(status).json({message:message})
+    res.status(status).json({message: message})
 })
 
 const MONGODB_URI = 'mongodb://admin:password@localhost:27017?authMechanism=DEFAULT'
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const {validationResult} = require('express-validator')
 const Post = require('../models/post')
 
@@ -31,13 +33,20 @@ exports.createPost = (req, res, next) => {
         //     })
     }
 
+    if (!req.file) {
+        const error = new Error('No image Provided')
+        error.statusCode = 422
+        throw error
+    }
+
+    const image = req.file.path
     const title = req.body.title;
     const content = req.body.content;
 
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: "asdasd",
+        imageUrl: image,
         creator: {
             name: "mosaab"
         },
@@ -78,5 +87,88 @@ exports.getPost = (req, res, next) => {
 
             next(err)
         })
+}
 
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed')
+        error.statusCode = 422
+        throw error
+    }
+
+    const postId = req.params.postId
+    const title = req.body.title
+    const content = req.body.content
+
+    let imageUrl = req.body.image
+
+    if (req.file) {
+        imageUrl = req.file.path
+    }
+
+    if (!imageUrl) {
+        const err = new Error('no file picked')
+        err.statusCode = 422
+        throw err
+    }
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Post Not found')
+                error.statusCode = 404
+                throw error
+            }
+
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl)
+            }
+
+            post.title = title
+            post.imageUrl = imageUrl
+            post.content = content
+            return post.save()
+        })
+        .then(result => {
+            res.status(200).json({message: 'Post Updated', post: result})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Post Not found')
+                error.statusCode = 404
+                throw error
+            }
+
+            clearImage(post.imageUrl)
+
+            return Post.findOneAndRemove(postId)
+        })
+        .then(result => {
+            res.status(200).json({message: 'Post Deleted'})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath)
+    fs.unlink(filePath, err => {
+        console.log(err)
+    })
 }
