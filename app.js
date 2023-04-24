2d9ce09bd9fb6098553c728b6824aecf9d24f576
Mosaab <musab.ehab@gmail.com>
2023-04-24 19:15:51 +0200

    add mongodb connection & static files & error handler

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const path = require('path')
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,5 +18,26 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use((err, req, res, next) => {
+    console.log(err)
+    const status = err.statusCode
+    const message = err.message
 
-app.listen(8080);
+    res.status(status).json({message:message})
+})
+
+const MONGODB_URI = 'mongodb://admin:password@localhost:27017?authMechanism=DEFAULT'
+
+mongoose
+    .connect(
+        MONGODB_URI, {
+            useNewUrlParser: true
+        }
+    )
+    .then(result => {
+        app.listen(8080)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
